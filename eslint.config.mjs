dba476cbf04b4f716ae1e6e37349a6084e07e1f3
Anthony Grullon <agrullon95@gmail.com>
2025-08-18 21:05:16 -0400

    chore: update ESLint and Prettier configurations to include .next directory and improve rule settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,31 @@ import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  pluginReact.configs.flat.recommended,
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended", "plugin:prettier/recommended"],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+  {
+    files: ["frontend/web/**/*.{js,jsx}"],
+    plugins: { react: pluginReact },
+    extends: ["plugin:react/recommended", "next/core-web-vitals"],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
 ]);
